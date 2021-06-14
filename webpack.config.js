0c23a8a1832e7721b6561b7079a189f14da5fbbd
Andrey Pechennikov <ap@devandy.me>
2021-06-14 21:03:52 +0600

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const webpack = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+module.exports = {
+  context: path.resolve('src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devtool: isDev ? 'source-map' : false,
+  target: isDev ? 'web' : 'browser-list',
+  devSerever: {
+    port: 4000,
+    open: isDev,
+    hot: isDev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: !isDev,
+        collapseWhitespace: !isDev
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        },
+        {
+          from: path.resolve(__dirname, 'src/img/*.**'),
+          to: ({context, absoluteFilename }) => {
+            return 'dest/src/[name][ext]'
+          }
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+    new ESLintPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
